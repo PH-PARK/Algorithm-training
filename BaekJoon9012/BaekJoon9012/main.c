c98fd148f581b7eb9a5d1520138522fd08785b34
PH Park <35191440+PH-PARK@users.noreply.github.com>
2018-09-30 17:42:39 +0900

    Basic Structure

---
+++ BaekJoon9012/BaekJoon9012/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#define maxLen 50;
+#define minLen 2;
+
+int getSting(char *buf, int maxLen);
+int checkSting(char *buf);
+
+char sting[maxLen];
+uint8_t NumofData;
+
+int main()
+{
+
+}
+
+int getString(char *buf, int maxLen)
+{
+
+}
+
+int checkSting(char *byf)
+{
+
+}
