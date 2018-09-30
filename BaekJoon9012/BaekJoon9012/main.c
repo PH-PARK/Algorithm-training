7a641794fff339b2da4a2f7061030c3c1340e668
PH Park <35191440+PH-PARK@users.noreply.github.com>
2018-09-30 17:56:11 +0900

    Main Structure

--- BaekJoon9012/BaekJoon9012/main.c
+++ BaekJoon9012/BaekJoon9012/main.c
@@ -7,12 +7,41 @@
 int getString(char *buf);
 int checkSting(char *buf);
 
-char sting[maxLen];
+char string[maxLen];
 uint8_t NumofData;
 
 int main()
 {
+	int i;
 
+	//puts("How many VPS?: ");
+	scanf_s("%d", &NumofData);
+
+	for (i = NumofData; i > 0 ; i--)
+	{
+		if (getSting(string) == 1)
+		{
+			puts("String input failed.");
+
+			return 1;
+		}
+
+		if (checkString(string) == 1)
+		{
+			puts("YES");
+		}
+		else if (checkString(string) == 0)
+		{
+			puts("NO");
+		}
+		else
+		{
+			puts("Something wrong...");
+			return 1;
+		}
+	}
+
+	return 0;
 }
 
 int getString(char *buf)
@@ -20,7 +49,7 @@ int getString(char *buf)
 	
 }
 
-int checkSting(char *byf)
+int checkSting(char *buf)
 {
 
 }
