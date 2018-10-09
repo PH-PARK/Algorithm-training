e8036a856663bcc102cfb8db7c85b2c699a61fcd
PH Park <35191440+PH-PARK@users.noreply.github.com>
2018-10-09 16:23:06 +0900

    LINUX WINDOW compatibility.
    #ifdef _WIN32
    	scanf_s("%d", &NumofData);
    #elif __linux__
    	scanf("%d", &NumofData);
    #endif

--- BaekJoon9012/BaekJoon9012/main.c
+++ BaekJoon9012/BaekJoon9012/main.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-
+	
 #define maxLen 50
 #define minLen 2
-
-//int getString(char *buf);
-//int checkString(char *buf);
 
 typedef char element;
 element stack[maxLen];
@@ -17,7 +14,11 @@ int main()
 	int i;
 
 	//puts("How many VPS?: ");
+#ifdef _WIN32
 	scanf_s("%d", &NumofData);
+#elif __linux__
+	scanf("%d", &NumofData);
+#endif
 
 	for (i = NumofData; i > 0; i--)
 	{
@@ -25,7 +26,7 @@ int main()
 	}
 
 	puts("종료합니다.");
-	scanf_s("%d", &NumofData);
+	system("pause");
 
 	return 0;
 }
@@ -38,61 +39,5 @@ void push(char item)
 char pop(void)
 {
 
-	return 
+	return;
 }
-/*
-int getString(char *buf)
-{
-	char temp;
-	int i;
-
-	for (i = 0; i < maxLen; i++ )
-	{
-		temp = getchar();
-
-		if (temp == '\n')
-		{
-			buf[i] = '\0';
-			return 0;
-		}
-	}
-
-	return 1;
-
-}
-
-int checkString(char *buf)
-{
-	char temp;
-	int i;
-	int j=0;
-
-	for (i = 0; i < maxLen; i++ )
-	{
-		temp = buf[i];
-		if (temp == 72)
-		{
-			i++;
-		}
-		else if (temp == 73)
-		{
-			i--;
-			j++;
-		}
-		if (temp = '\n')
-		{
-			if (i == 0 && j!=0)
-			{
-			
-				return 1;
-			}
-			else if(i != 0)
-			{
-
-				return 0;
-			}
-		}
-	}
-
-	return 0;
-}*/
