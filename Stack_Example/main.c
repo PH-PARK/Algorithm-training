a43f64ee6f55ee6542569a103ebdbc104c59901a
PH Park <35191440+PH-PARK@users.noreply.github.com>
2018-10-09 16:21:14 +0900

    Stack Example Complete
    Just copied it.

--- Stack_Example/main.c
+++ Stack_Example/main.c
@@ -7,24 +7,39 @@ typedef int element;
 element stack[MAX_STACK_SIZE];
 int top = -1;
 
+void push(int item);
+int pop(void);
+int is_empty(void);
+int is_full(void);
+
 int main()
 {
 	int num;
 
 	while (1)
 	{
-		scanf("%d", &num);
-		if (num == -1)
+		puts("Enter Item.");
+		scanf_s("%d", &num);
+
+		if (num == -1) {
+			
+			puts("Out Items.");
 			break;
+		}
+
 		push(num);
 
 	}
 
 	while (!is_empty())
 	{
+
 		num = pop();
-		printf("%d", num);
+		printf("%d\t", num);
 	}
+
+	system("pause");
+
 	return 0;
 }
 void push(element item)
@@ -32,6 +47,8 @@ void push(element item)
 	assert(!is_full());
 
 	stack[++top] = item;
+
+	return;
 }
 
 int pop()
@@ -41,11 +58,11 @@ int pop()
 	return stack[top--];
 }
 
-int is_empty()
+int is_empty(void )
 {
 	return (top == -1);
 }
-int is_full()
+int is_full(void )
 {
 	return (top == (MAX_STACK_SIZE - 1));
 }
